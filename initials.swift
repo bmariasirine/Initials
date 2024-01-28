3d4a35aa2060221ee3ff023e4c02abc78d736ab3
Maria <148819454+bmariasirine@users.noreply.github.com>
2024-01-28 11:42:51 +0100

    Create initials.swift

---
+++ initials.swift
@@ -0,0 +1,7 @@
+print("M   M     A     RRRR    IIIII     A  ")
+print("MM MM    A A    R   R     I      A A ")
+print("MM MM   A   A   R   R     I     A   A")
+print("M M M   AAAAA   RRRR      I     AAAAA")
+print("M   M   A   A   R R       I     A   A")
+print("M   M   A   A   R  R      I     A   A")
+print("M   M   A   A   R   R   IIIII   A   A")
